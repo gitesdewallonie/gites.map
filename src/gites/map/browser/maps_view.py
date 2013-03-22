892f63b6097e6001359999ae55c3c3e71e43c001
Jean-François Roche <jfroche@affinitic.be>
2013-03-22 16:55:20 +0100

    fix condition for Products.Maps js ##

--- src/gites/map/browser/maps_view.py
+++ src/gites/map/browser/maps_view.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from zope.component import getMultiAdapter
 from plone.memoize.instance import memoize
+from Products.Maps.browser.map import DefaultMapView
 from monet.mapsviewlet.browser.maps_view import MapsView as BaseMapsView
 from monet.mapsviewlet.interfaces import IMonetMapsEnabledContent
 
@@ -14,3 +15,15 @@ class MapsView(BaseMapsView):
         context_state = getMultiAdapter((context, self.request), name=u'plone_context_state')
         isViewTemplate = context_state.is_view_template()
         return not isViewTemplate and IMonetMapsEnabledContent.providedBy(context)
+
+
+class MonetMapView(DefaultMapView):
+
+    @property
+    def enabled(self):
+        if self.map is None:
+            return False
+        context = self.context
+        context_state = getMultiAdapter((context, self.request), name=u'plone_context_state')
+        isViewTemplate = context_state.is_view_template()
+        return isViewTemplate is False
