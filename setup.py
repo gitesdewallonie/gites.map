c7a287222deada2c3ea2862a586fbbe2aaca5d90
Jean-François Roche <jfroche@affinitic.be>
2013-04-08 12:20:10 +0200

    unconfigure monet view/viewlet - refs #4843

--- setup.py
+++ setup.py
@@ -19,16 +19,14 @@ setup(name='gites.map',
       long_description=long_description,
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Programming Language :: Python",
-        ],
+      classifiers=["Programming Language :: Python"],
       keywords='',
       author='',
       author_email='',
       url='http://svn.plone.org/svn/collective/',
       license='gpl',
       packages=find_packages('src'),
-      package_dir = {'': 'src'},
+      package_dir={'': 'src'},
       namespace_packages=['gites'],
       include_package_data=True,
       zip_safe=False,
@@ -36,11 +34,4 @@ setup(name='gites.map',
           'setuptools',
           'anyjson',
           'z3c.json',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+          'z3c.unconfigure'])
