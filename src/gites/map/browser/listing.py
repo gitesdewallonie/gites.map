f87389a59f858e5d645973a30b31700088065e36
Jean-François Roche <jfroche@affinitic.be>
2013-04-08 23:25:54 +0200

    make maplisting available for other views - refs #4859

--- src/gites/map/browser/listing.py
+++ src/gites/map/browser/listing.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 from five import grok
-from zope.interface import implements
-from gites.core.content.interfaces import IPackage
-from gites.core.browser.interfaces import IPackageView
+from zope.interface import implements, Interface
 from gites.map.browser.viewlets import GitesMapViewlet
 
 
 class MapListing(GitesMapViewlet, grok.View):
-    implements(IPackageView)
-    grok.context(IPackage)
+    implements(Interface)
+    grok.context(Interface)
     grok.name('update_map_listing')
 
     def __init__(self, context, request):
