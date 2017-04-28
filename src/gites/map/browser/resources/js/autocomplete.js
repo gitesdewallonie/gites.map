d87eba18195374ffae1c2001e85a57b28c7a5add
Alain Meurant <alain.meurant@affinitic.be>
2017-04-28 19:25:57 +0200

    Map et moteur de recherche —> autocomplete
    - forcer lang à fr pou test

--- src/gites/map/browser/resources/js/autocomplete.js
+++ src/gites/map/browser/resources/js/autocomplete.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
 
     var countryRestrict = {'country': 'be'};
     var field = document.getElementById('nearto-autocomplete');
-    var language = field.lang;
+    var language = 'fr';   /*field.lang;*/
     autocomplete = new google.maps.places.Autocomplete(
       field,
       {
